b70254577951b8323fd1d84ca6b801281654297d
phancao2712 <phancaoa1@gmail.com>
2024-07-08 14:02:56 +0700

    setup tailwind

--- react_app/src/main.tsx
+++ react_app/src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import Login from './page/login';
 
+const router = createBrowserRouter([
+    {
+        path: "/login",
+        element: <Login />,
+    },
+]);
+import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
 )
